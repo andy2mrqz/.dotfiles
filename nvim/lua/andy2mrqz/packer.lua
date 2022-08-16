586c8124747fecb646f42bf0bb39834a7a155e47
Andrew Marquez <andy2mgcc@gmail.com>
2022-08-16 01:49:09 -0600

    (neovim) Fix packer auto-install on new system

--- nvim/lua/andy2mrqz/packer.lua
+++ nvim/lua/andy2mrqz/packer.lua
@@ -3,9 +3,14 @@
 --
 local install_path = vim.fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = vim.fn.system(
-    "git clone --depth 1 https://github.com/wbthomason/packer.nvim" .. install_path
-  )
+  PACKER_BOOTSTRAP = vim.fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path
+  }
   vim.cmd [[packadd packer.nvim]]
 end
 
