506318114c9f0eb071de915c8e93db56a8034382
Andrew Marquez <andy2mgcc@gmail.com>
2022-08-28 01:22:19 -0600

    (neovim) Refactor LSP setup

--- nvim/lua/andy2mrqz/lsp/handlers.lua
+++ nvim/lua/andy2mrqz/lsp/handlers.lua
@@ -2,52 +2,8 @@ local nnoremap = require("andy2mrqz.keymaps").nnoremap
 
 local M = {}
 
-M.setup = function()
-  local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn", text = "" },
-    { name = "DiagnosticSignHint", text = "" },
-    { name = "DiagnosticSignInfo", text = "" },
-  }
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(
-      sign.name,
-      { texthl = sign.name, text = sign.text, numhl = "" }
-    )
-  end
-
-  local config = {
-    virtual_text = false,
-    signs = {
-      active = signs
-    },
-    update_in_insert = true,
-    underline = true,
-    severity_sort = true,
-    float = {
-      focusable = false,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
-    },
-  }
-
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-    width = 60,
-  })
-
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-    width = 60,
-  })
-
-end
-
--- Configure lsp Keymaps
-local function lsp_keymaps(bufnr)
+M.on_attach = function(client, bufnr)
+  -- Configure lsp Keymaps
   local bufopts = { silent = true, buffer = bufnr }
   nnoremap("gD", vim.lsp.buf.declaration, bufopts)
   nnoremap("gd", vim.lsp.buf.definition, bufopts)
@@ -59,10 +15,6 @@ local function lsp_keymaps(bufnr)
   nnoremap("gi", vim.lsp.buf.implementation, bufopts)
   nnoremap("<C-k>", vim.lsp.buf.signature_help, bufopts)
   nnoremap("<space>f", vim.lsp.buf.formatting, bufopts)
-end
-
-M.on_attach = function(client, bufnr)
-  lsp_keymaps(bufnr)
 end
 
 
