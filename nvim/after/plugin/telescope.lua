26d86c857268556f39e91376000f0893490c7b97
Andrew Marquez <andy2mgcc@gmail.com>
2022-09-25 20:28:12 -0600

    (neovim) Ignore .git files in searches

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,4 +1,4 @@
-local status_ok, telescope = pcall(require, 'telescope')
+local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
   print("telescope failed to load")
   return
@@ -6,19 +6,21 @@ end
 
 local actions = require("telescope.actions")
 
-telescope.setup {
-  defaults = {
-    winblend = 5,
-    path_display = { shorten = { len = 1, exclude = { 1, -1 } } },
+telescope.setup({
+	defaults = {
+		winblend = 5,
+		path_display = { shorten = { len = 1, exclude = { 1, -1 } } },
+		file_ignore_patterns = {
+			"^.git/",
+		},
+		mappings = {
+			i = {
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
 
-    mappings = {
-      i = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-      }
-    }
-  },
-}
+				["<C-n>"] = actions.cycle_history_next,
+				["<C-p>"] = actions.cycle_history_prev,
+			},
+		},
+	},
+})
