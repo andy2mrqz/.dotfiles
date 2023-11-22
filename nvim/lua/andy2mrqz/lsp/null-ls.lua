5e73fe93e291bbdb56e0384edbed38f7efb7bd21
Andrew Marquez <andy2mgcc@gmail.com>
2023-11-21 21:59:57 -0700

    (nvim) Configure shellcheck to run for zsh files

--- nvim/lua/andy2mrqz/lsp/null-ls.lua
+++ nvim/lua/andy2mrqz/lsp/null-ls.lua
@@ -1,22 +1,25 @@
 local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
-  print("null-ls failed to load")
-  return
+	print("null-ls failed to load")
+	return
 end
 
 local lsp_handlers = require("andy2mrqz.lsp.handlers")
 
 null_ls.setup({
-  debug = false,
-  sources = {
-    null_ls.builtins.formatting.prettier,
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.shfmt.with({
-      extra_args = { "-ci", "-s", "-bn" },
-    }),
-    null_ls.builtins.formatting.rustfmt,
-    null_ls.builtins.diagnostics.shellcheck,
-    null_ls.builtins.diagnostics.mypy,
-  },
-  on_attach = lsp_handlers.on_attach,
+	debug = false,
+	sources = {
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.shfmt.with({
+			extra_args = { "-ci", "-s", "-bn" },
+		}),
+		null_ls.builtins.formatting.rustfmt,
+		null_ls.builtins.diagnostics.shellcheck.with({
+			extra_args = { "--shell=bash" },
+			filetypes = { "zsh", "sh", "bash" },
+		}),
+		null_ls.builtins.diagnostics.mypy,
+	},
+	on_attach = lsp_handlers.on_attach,
 })
