622f39ca8ae3e6932d4217386ded19d4a4046f71
Andrew Marquez <andy2mgcc@gmail.com>
2024-03-21 09:31:56 -0600

    (macos) Add echo statements for context while running

--- macos/bin/ecs-execute.sh
+++ macos/bin/ecs-execute.sh
@@ -4,4 +4,8 @@ TARGET="$1"
 
 TASK_ARN=$(aws ecs list-tasks --cluster "$TARGET" --service-name "$TARGET" --desired-status RUNNING --query 'taskArns[0]' --output text)
 
+echo "Running the following command:"
+echo "aws ecs execute-command --cluster $TARGET --task $TASK_ARN --container $TARGET --command \"/bin/bash\" --interactive"
+
 aws ecs execute-command --cluster "$TARGET" --task "$TASK_ARN" --container "$TARGET" --command "/bin/bash" --interactive
+
