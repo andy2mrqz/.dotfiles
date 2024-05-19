e0798a369d3144904483a5ac318e5c3fdb3e75b5
Andrew Marquez <andy2mgcc@gmail.com>
2024-05-18 23:22:32 -0600

    (nvim) Update vim.diagnostics to use the new API

--- nvim/lua/andy2mrqz/lsp/config.lua
+++ nvim/lua/andy2mrqz/lsp/config.lua
@@ -1,17 +1,19 @@
-local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
-}
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
-
 vim.diagnostic.config({
 	virtual_text = true,
 	signs = {
-		active = signs,
+		-- active = signs,
+		text = {
+			[vim.diagnostic.severity.ERROR] = "",
+			[vim.diagnostic.severity.WARN] = "",
+			[vim.diagnostic.severity.HINT] = "",
+			[vim.diagnostic.severity.INFO] = "",
+		},
+		numhl = {
+			[vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
+			[vim.diagnostic.severity.WARN] = "DiagnosticSignWarn",
+			[vim.diagnostic.severity.HINT] = "DiagnosticSignHint",
+			[vim.diagnostic.severity.INFO] = "DiagnosticSignInfo",
+		},
 	},
 	update_in_insert = true,
 	underline = true,
