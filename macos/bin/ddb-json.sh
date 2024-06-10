19f55099a37d3a7c12ec99b65d7a09b6d5a92580
Andrew Marquez <andy2mgcc@gmail.com>
2024-06-09 22:25:46 -0600

    (macos) Switch from chrome to arc

--- macos/bin/ddb-json.sh
+++ macos/bin/ddb-json.sh
@@ -12,7 +12,7 @@
 # @raycast.author Andrew Marquez
 
 osascript <<EOF
-tell application "Google Chrome"
+tell application "Arc"
     tell the active tab of its first window
         execute javascript "document.evaluate('//span[text()=\"JSON view\"]/parent::*',document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue.click(),document.evaluate('//span[text()=\"View DynamoDB JSON\"]/parent::*',document,null,XPathResult.FIRST_ORDERED_NODE_TYPE,null).singleNodeValue.click();"
     end tell
