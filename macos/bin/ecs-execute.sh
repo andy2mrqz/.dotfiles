84815fad7ccdcf5b5b011834ff962bc4c4bda100
Andrew Marquez <andy2mgcc@gmail.com>
2025-03-21 11:53:15 -0600

    (macos) Change comment of command from /bin/bash to /bin/sh in ecs-execute.sh script
    [commit message generated by ai]

--- macos/bin/ecs-execute.sh
+++ macos/bin/ecs-execute.sh
@@ -5,7 +5,7 @@ TARGET="$1"
 TASK_ARN=$(aws ecs list-tasks --cluster "$TARGET" --service-name "$TARGET" --desired-status RUNNING --query 'taskArns[0]' --output text)
 
 echo "Running the following command:"
-echo "aws ecs execute-command --cluster $TARGET --task $TASK_ARN --container $TARGET --command \"/bin/bash\" --interactive"
+echo "aws ecs execute-command --cluster $TARGET --task $TASK_ARN --container $TARGET --command \"/bin/sh\" --interactive"
 
 aws ecs execute-command --cluster "$TARGET" --task "$TASK_ARN" --container "$TARGET" --command "/bin/sh" --interactive
 
