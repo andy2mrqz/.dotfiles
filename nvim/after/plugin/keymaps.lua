4d51e4377cc43a5fc13b4310fe84f383e2ebbaaa
Andrew Marquez <andy2mgcc@gmail.com>
2025-12-05 11:52:14 -0700

    (nvim) Integrate scrolling with neoscroll better by mapping to neoscroll targets

--- nvim/after/plugin/keymaps.lua
+++ nvim/after/plugin/keymaps.lua
@@ -1,3 +1,4 @@
+local inoremap = require("andy2mrqz.keymaps").inoremap
 local nnoremap = require("andy2mrqz.keymaps").nnoremap
 local tnoremap = require("andy2mrqz.keymaps").tnoremap
 
@@ -10,6 +11,11 @@ tnoremap("<esc>", "<C-\\><C-n>") -- <esc> goes to normal mode in terminal
 -- Buffer navigation
 nnoremap("<S-l>", ":bn<cr>")
 nnoremap("<S-h>", ":bp<cr>")
+
+nnoremap("<ScrollWheelUp>", "<C-y>")
+inoremap("<ScrollWheelUp>", "<C-y>")
+nnoremap("<ScrollWheelDown>", "<C-e>")
+inoremap("<ScrollWheelDown>", "<C-e>")
 
 -- Clear search highlights
 nnoremap("q,", ':let @/ = ""<cr>')
