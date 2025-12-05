cf0e3b6cb817551cbd4e3ff6ab99043a568b2a03
Andrew Marquez <andy2mgcc@gmail.com>
2025-12-05 11:53:42 -0700

    (nvim) Update telescope settings (buffers, result sorting, path display, etc.)

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -5,11 +5,33 @@ if not status_ok then
 end
 
 telescope.setup({
+	pickers = {
+		buffers = {
+			mappings = {
+				n = {
+					["dd"] = "delete_buffer",
+				},
+			},
+		},
+	},
 	defaults = {
-		winblend = 5,
-		path_display = { shorten = { len = 1, exclude = { 1, -1 } } },
-		file_ignore_patterns = {
-			"^.git/",
+		sorting_strategy = "ascending",
+		-- layout_strategy = "center",
+		-- layout_config = { anchor = "S", height = 0.65 },
+		winblend = 15,
+		path_display = { "filename_first" },
+		file_ignore_patterns = { "^.git/" },
+		dynamic_preview_title = true,
+		vimgrep_arguments = {
+			"rg",
+			"--color=never",
+			"--no-heading",
+			"--with-filename",
+			"--line-number",
+			"--column",
+			"--hidden",
+			"--ignore-case",
+			"--sort=path", -- match case with vscode search (alphabetical)
 		},
 		mappings = {
 			i = {
